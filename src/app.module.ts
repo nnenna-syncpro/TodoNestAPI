cdb5cc4d1dbad9d8a8605a4eae966cc67f24ea45
esot0001 <esot0001@algonquinlive.com>
2024-09-13 20:32:35 -0400

    feat: adds todo module, service, and controller and adds typeorm module

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TodoModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'todo.db',
+      entities: [__dirname, '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
